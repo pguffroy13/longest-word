1e8360bde3f032f4ccc9c1b7595d62c97e1ae216
Philippe Guffroy <pguffroy13@gmail.com>
2020-01-21 17:22:35 +0100

    Add Procfile to prepare Heroku deployment

--- wsgi.py
+++ wsgi.py
@@ -4,6 +4,11 @@ from game import Game
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    game = Game()
+    return render_template('home.html', grid=game.grid)
+
 @app.route('/check', methods=["POST"])
 def check():
     game = Game()
